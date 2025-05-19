477d790fc887dc1f786c4e47a09d0dca9a44aca0
burakcelikkiran <80842957+burakcelikkiran@users.noreply.github.com>
2025-05-19 19:58:45 +0300

    görsel revizesi

--- popup.php
+++ popup.php
@@ -27,7 +27,7 @@ if ($showPopup) {
                 </header>
 
                 <main class="modal__content" id="modal-1-content">
-                        <img alt="" src="/doc/flbovefat.jpg">
+                        <img alt="" src="/doc/ayse-erdil-vefat.png">
                 </main>
 
             </div>

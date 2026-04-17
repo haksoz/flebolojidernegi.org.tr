5247bea239c99533b0a00e6f88f9ab44206a1be9
burakcelikkiran <80842957+burakcelikkiran@users.noreply.github.com>
2026-04-17 16:40:49 +0300

    görünüm tip revize edildi.

--- script.php
+++ script.php
@@ -102,10 +102,10 @@
                                         navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
                                         responsive: {
                                             0: {
-                                                items: 2
+                                                items: 1
                                             },
                                             600: {
-                                                items: 2
+                                                items: 1
                                             },
                                             1000: {
                                                 items: 4
